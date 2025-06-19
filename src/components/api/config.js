f0bf9ea607e16c1b805f3c582419db62987eb7ed
jeevanjames2000 <jeevanjames2000@gmail.com>
2025-06-19 14:45:00 +0530

    address issues

--- src/components/api/config.js
+++ src/components/api/config.js
@@ -1,6 +1,6 @@
 const config = {
-  // api_url: "http://localhost:5000",
-  api_url: "https://api.meetowner.in",
+  api_url: "http://localhost:5000",
+  // api_url: "https://api.meetowner.in",
 };
 
 export default config;
