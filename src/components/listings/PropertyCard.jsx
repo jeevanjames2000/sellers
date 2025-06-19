41dd890dbda8f50b9177df9d386c1c934d11046a
jeevanjames2000 <jeevanjames2000@gmail.com>
2025-06-19 14:57:09 +0530

    sd

--- src/components/listings/PropertyCard.jsx
+++ src/components/listings/PropertyCard.jsx
@@ -237,11 +237,6 @@ const PropertyCard = ({
                   <span className="text-sm">{location}</span>
                 </div>
                 <div className="flex items-center text-gray-600 gap-2">
-                  {showFurnishedStatus && furnished_status && (
-                    <span className="text-sm">
-                      {furnished_status} Furnished
-                    </span>
-                  )}
                   <span className="text-sm">{facing} Facing</span>
                 </div>
                 <div className="flex flex-wrap items-center gap-3 py-1">
