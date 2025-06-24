0c0d456779a9443b0f7b082673832cf59a7c7591
jeevanjames2000 <jeevanjames2000@gmail.com>
2025-06-24 11:04:12 +0530

    sd

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import Header from "@/components/header/Header";
 import ReduxProvider from "../store/provider";
 import { Toaster } from "react-hot-toast";
 import ProfileCheckProvider from "@/components/services/ProfileCheckProvider";
-
+import Script from "next/script";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -23,9 +23,29 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Tag Manager Script */}
+        <Script id="gtm-head" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-P8HMN9BJ');
+          `}
+        </Script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-P8HMN9BJ"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <ProfileCheckProvider>
           <ReduxProvider>
             <Header />
